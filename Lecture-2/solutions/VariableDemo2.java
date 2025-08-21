a1d17b3f6cbef87dd6c93a519a5386f18b7ce7b7
tech4commun <pedaltriangle@gmail.com>
2025-08-21 20:33:54 +0530

    Add VariableDemo2.java to demonstrate variable initialization and usage in Java

---
+++ Lecture-2/solutions/VariableDemo2.java
@@ -0,0 +1,16 @@
+public class VariableDemo2 {
+    public static void main(String[] args) {
+        // Direct initialization
+        int a = 5;
+
+        // Using another variable
+        int b = a;
+
+        // Using an expression
+        int c = a + b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
