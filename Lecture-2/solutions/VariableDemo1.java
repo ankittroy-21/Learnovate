23c38bd6688d6a04a935d54b87cc9224a7b3c645
tech4commun <pedaltriangle@gmail.com>
2025-08-21 20:33:29 +0530

    Add VariableDemo1.java to demonstrate variable declaration and initialization in Java

---
+++ Lecture-2/solutions/VariableDemo1.java
@@ -0,0 +1,12 @@
+public class VariableDemo1 {
+    public static void main(String[] args) {
+        // Declare first, then initialize
+        int num;
+        num = 10;
+        System.out.println("Number after initialization: " + num);
+
+        // Declare + Initialize in one step
+        int value = 20;
+        System.out.println("Declared + Initialized: " + value);
+    }
+}
