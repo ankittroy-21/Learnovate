b269d2b05f12d3021667b37843399fbe61364353
tech4commun <pedaltriangle@gmail.com>
2025-08-21 20:34:24 +0530

    Add VariableDemo4.java to demonstrate the use of final variables in Java

---
+++ Lecture-2/solutions/VariableDemo4.java
@@ -0,0 +1,8 @@
+public class VariableDemo4 {
+    public static void main(String[] args) {
+        final int MAX_VALUE = 100;
+        System.out.println("MAX_VALUE = " + MAX_VALUE);
+
+        // MAX_VALUE = 200; // ❌ Error: cannot assign a value to final variable
+    }
+}
