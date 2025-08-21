94ec3d84c82fdd41527a21ce917c9693561e2e24
tech4commun <pedaltriangle@gmail.com>
2025-08-21 20:34:10 +0530

    Add VariableDemo3.java to demonstrate variable initialization in Java

---
+++ Lecture-2/solutions/VariableDemo3.java
@@ -0,0 +1,10 @@
+public class VariableDemo3 {
+    public static void main(String[] args) {
+        int x;
+        // System.out.println(x); // ❌ Compile-time Error: variable x might not have been initialized
+
+        // Correct way: initialize before use
+        x = 15;
+        System.out.println("x = " + x);
+    }
+}
