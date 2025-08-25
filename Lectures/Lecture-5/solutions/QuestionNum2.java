983a36604b7a54cc23988eb4ece89ec0e30d1dd8
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-25 23:28:05 +0530

    Create QuestionNum2.java

---
+++ Lectures/Lecture-5/solutions/QuestionNum2.java
@@ -0,0 +1,11 @@
+public class QuestionNum2 {
+    public static void main(String[] args) {
+        int age = 70; // Change this value to test different ages
+
+        // Check eligibility using relational and logical operators
+        boolean isEligible = (age < 12) || (age > 65);
+
+        // Output
+        System.out.println("Eligible for discount: " + isEligible);
+    }
+}
