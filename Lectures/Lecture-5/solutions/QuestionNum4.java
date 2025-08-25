0bc7b49e8e9119fcf409061badb20d35aaeff3cb
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-25 23:29:04 +0530

    Create QuestionNum4.java

---
+++ Lectures/Lecture-5/solutions/QuestionNum4.java
@@ -0,0 +1,12 @@
+public class QuestionNum4 {
+    public static void main(String[] args) {
+        // Pre-increment
+        int a = 10;
+        System.out.println("Pre-increment (a=10): " + (++a));
+
+        // Post-increment
+        int b = 10;
+        System.out.println("Post-increment (b=10): " + (b++));
+        System.out.println("Final value of b: " + b);
+    }
+}
