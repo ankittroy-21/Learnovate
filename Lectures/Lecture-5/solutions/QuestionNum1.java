e9c8bbb1ad57266af6ecec3ce2692fab73908349
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-25 23:27:35 +0530

    Create QuestionNum1.java

---
+++ Lectures/Lecture-5/solutions/QuestionNum1.java
@@ -0,0 +1,20 @@
+public class QuestionNum1 {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 8;
+
+        // Calculations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Output
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
