b612286decc20efbf036612790cbb6fabcb254d3
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-27 00:12:01 +0530

    Create QuestionNum3.java

---
+++ Lectures/Lecture-6/solutions/QuestionNum3.java
@@ -0,0 +1,11 @@
+public class QuestionNum3 {
+    public static void main(String[] args) {
+        int i = 1; // Initialize counter variable
+        
+        // While loop to print numbers from 1 to 10
+        while (i <= 10) { // Condition checked before each iteration
+            System.out.println(i); // Print current number
+            i++; // Increment counter
+        }
+    }
+}
