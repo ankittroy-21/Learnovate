71eae09dc10040fc9fc62153d211c4fbd84d430d
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-27 00:16:22 +0530

    Create QuestionNum8.java

---
+++ Lectures/Lecture-6/solutions/QuestionNum8.java
@@ -0,0 +1,10 @@
+public class QuestionNum8 {
+    public static void main(String[] args) {
+        int number = 7; // Number for multiplication table
+        
+        // For loop to print multiplication table
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
