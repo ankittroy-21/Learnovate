6aa9a6b504ee1db6d1390dc911bdd3d481f2c67c
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-27 00:11:17 +0530

    Update QuestionNum2.java

--- Lectures/Lecture-6/solutions/QuestionNum2.java
+++ Lectures/Lecture-6/solutions/QuestionNum2.java
@@ -1,13 +1,32 @@
 public class QuestionNum2 {
     public static void main(String[] args) {
-        int number = -5;
+        int dayNumber = 3; // Predefined day number (1-7)
         
-        if (number > 0) {
-            System.out.println("Positive");
-        } else if (number < 0) {
-            System.out.println("Negative");
-        } else {
-            System.out.println("Zero");
+        // Switch statement to convert number to day name
+        switch (dayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day number"); // Handles numbers outside 1-7
         }
     }
 }
