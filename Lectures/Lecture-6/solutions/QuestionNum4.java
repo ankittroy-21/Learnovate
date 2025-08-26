081a3dc26ffb8ebdf8a19ff429bf5c68de011490
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-27 00:12:39 +0530

    Create QuestionNum4.java

---
+++ Lectures/Lecture-6/solutions/QuestionNum4.java
@@ -0,0 +1,12 @@
+public class QuestionNum4 {
+    public static void main(String[] args) {
+        int sum = 0; // Initialize sum variable
+        
+        // For loop to calculate sum of numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            sum += i; // Add current number to sum
+        }
+        
+        System.out.println("Sum of numbers from 1 to 100: " + sum); // Print result
+    }
+}
