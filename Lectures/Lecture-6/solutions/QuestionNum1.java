b4faafedfbf8e123678592983c0b6e254ff2813e
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-27 00:10:39 +0530

    Update QuestionNum1.java

--- Lectures/Lecture-6/solutions/QuestionNum1.java
+++ Lectures/Lecture-6/solutions/QuestionNum1.java
@@ -1,12 +1,14 @@
 public class QuestionNum1 {
     public static void main(String[] args) {
-        int num1 = 15;
-        int num2 = 4;
+        int number = -5; // Predefined number to check
         
-        System.out.println("Sum: " + (num1 + num2));
-        System.out.println("Difference: " + (num1 - num2));
-        System.out.println("Product: " + (num1 * num2));
-        System.out.println("Quotient: " + (num1 / num2));
-        System.out.println("Remainder: " + (num1 % num2));
+        // If-else statement to check if number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("Positive"); // Executes if number is greater than 0
+        } else if (number < 0) {
+            System.out.println("Negative"); // Executes if number is less than 0
+        } else {
+            System.out.println("Zero"); // Executes if number equals 0
+        }
     }
 }
