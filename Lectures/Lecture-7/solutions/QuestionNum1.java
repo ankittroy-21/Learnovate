23666c5bb92dbbbeab9ad95c64cb5f665a255a8a
Ankit Roy <82595330+ankittroy-21@users.noreply.github.com>
2025-08-30 21:10:17 +0530

    Create QuestionNum1.java

---
+++ Lectures/Lecture-7/solutions/QuestionNum1.java
@@ -0,0 +1,14 @@
+public class QuestionNum1 {
+    
+    // Method to print welcome message
+    // This method takes no parameters and returns nothing (void)
+    public static void printWelcome() {
+        System.out.println("Welcome to Java Programming!");
+    }
+    
+    // Main method where program execution begins
+    public static void main(String[] args) {
+        // Calling the printWelcome method
+        printWelcome();
+    }
+}
